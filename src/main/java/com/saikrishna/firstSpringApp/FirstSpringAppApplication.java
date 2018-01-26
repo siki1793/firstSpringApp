3afd844bf19fa8aa648bb5100d3965ad972b6ee5
siki1793 <saikrishna1793.sk@gmail.com>
2018-01-26 20:02:32 +0530

    initial commit of spring to git

---
+++ src/main/java/com/saikrishna/firstSpringApp/FirstSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.saikrishna.firstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+}
